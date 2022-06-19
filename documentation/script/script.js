4ede7c9ac05c368c3d0ff5d195eefc1b9f7387b4
ebongmfoniso <mfonisoischris@gmail.com>
2022-06-19 13:54:17 -0700

    documentations added

---
+++ documentation/script/script.js
@@ -0,0 +1,49 @@
+$('document').ready(function(){
+
+    if(window.location.hash){
+        const elem= window.location.hash.replace('#', ''); 
+        scroll(elem.trim())
+    }
+
+    function scroll(elem){
+        const elemList= document.querySelectorAll(`[data-scroll]`)
+
+        elemList.forEach(item=> {
+            if(item.dataset.scroll===elem){
+                item.classList.add('active')
+            }
+            else{
+                item.classList.remove('active')
+
+            }
+        })
+
+    
+        window.location.hash=elem;
+        
+        $('html,.main').animate({
+            scrollTop: $(`[data-tag=${elem}]`).offset().top
+        }, '1000')
+        
+    }
+
+    $('[data-scroll=introduction]').click(function(){
+        scroll('introduction')
+    })
+    $('[data-scroll=packages]').click(function(){
+        scroll('packages')
+    })
+    $('[data-scroll=components]').click(function(){
+        scroll('components')
+    })
+    $('[data-scroll=image]').click(function(){
+        scroll('image')
+    })
+    $('[data-scroll=contact_form]').click(function(){
+        scroll('contact_form')
+    })
+    $('[data-scroll=installation]').click(function(){
+        scroll('installation')
+    })
+
+})
